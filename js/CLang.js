6379e973f1b9079b9f7c5d608752f3edf9b821d3
jitam <jitender.sridhar@gmail.com>
2020-12-28 19:09:38 +0530

    first commit

---
+++ js/CLang.js
@@ -0,0 +1,21 @@
+TEXT_MONEY     = "MONEY";
+TEXT_PLAY      = "PLAY";
+TEXT_BET       = "BET";
+TEXT_COIN      = "COIN";
+TEXT_MAX_BET   = "MAX BET";
+TEXT_INFO      = "INFO";
+TEXT_LINES     = "LINES";
+TEXT_SPIN      = "SPIN";
+TEXT_WIN       = "WIN";
+TEXT_FREESPIN  = "Free Spin";
+TEXT_HELP_WILD = " ";
+TEXT_CREDITS_DEVELOPED = "DEVELOPED BY";
+TEXT_CURRENCY = "$";
+TEXT_PRELOADER_CONTINUE = "START";
+
+TEXT_SHARE_IMAGE = "200x200.jpg";
+TEXT_SHARE_TITLE = "Congratulations!";
+TEXT_SHARE_MSG1 = "You collected <strong>";
+TEXT_SHARE_MSG2 = " points</strong>!<br><br>Share your score with your friends!";
+TEXT_SHARE_SHARE1 = "My score is ";
+TEXT_SHARE_SHARE2 = " points! Can you do better?";
---
+++ js/settings.js
@@ -0,0 +1,76 @@
+var CANVAS_WIDTH = 1500;
+var CANVAS_HEIGHT = 640;
+
+var EDGEBOARD_X = 300;
+var EDGEBOARD_Y = 0;
+
+var FPS_TIME      = 1000/24;
+var DISABLE_SOUND_MOBILE = false;
+var FONT_GAME = "arialbold";
+
+var STATE_LOADING = 0;
+var STATE_MENU    = 1;
+var STATE_HELP    = 1;
+var STATE_GAME    = 3;
+
+var GAME_STATE_IDLE         = 0;
+var GAME_STATE_SPINNING     = 1;
+var GAME_STATE_SHOW_ALL_WIN = 2;
+var GAME_STATE_SHOW_WIN     = 3;
+
+var REEL_STATE_START   = 0;
+var REEL_STATE_MOVING = 1;
+var REEL_STATE_STOP    = 2;
+
+var ON_MOUSE_DOWN = 0;
+var ON_MOUSE_UP   = 1;
+var ON_MOUSE_OVER = 2;
+var ON_MOUSE_OUT  = 3;
+var ON_DRAG_START = 4;
+var ON_DRAG_END   = 5;
+
+var REEL_OFFSET_X = 380;
+var REEL_OFFSET_Y = 84;
+
+var NUM_REELS = 5;
+var NUM_ROWS = 3;
+var NUM_SYMBOLS = 15;
+var WILD_SYMBOL = 15;
+var WILD_SYMBOLS = [10,11,12,13];
+var BONUS_SYMBOL = 14;
+var FREESPIN_SYMBOL = 15;
+
+var NUM_PAYLINES = 20;
+var SYMBOL_SIZE = 140;
+var SPACE_BETWEEN_SYMBOLS = 10;
+var MAX_FRAMES_REEL_EASE = 16;
+var MIN_REEL_LOOPS;
+var REEL_DELAY;
+var REEL_START_Y = REEL_OFFSET_Y - (SYMBOL_SIZE * 3);
+var REEL_ARRIVAL_Y = REEL_OFFSET_Y + (SYMBOL_SIZE * 3);
+var TIME_SHOW_WIN;
+var TIME_SHOW_ALL_WINS;
+var MIN_BET = 0.05;
+var MAX_BET = 0.5;
+var TOTAL_MONEY;
+var WIN_OCCURRENCE;
+var SLOT_CASH;
+var MIN_WIN;
+var PAYTABLE_VALUES;
+
+var ENABLE_FULLSCREEN = false;
+var ENABLE_CHECK_ORIENTATION;
+var SHOW_CREDITS = false;
+
+var getParams = function (url) {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
